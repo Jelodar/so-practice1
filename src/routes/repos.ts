7f3427e26e8f9de9b8bb46ee4c747586e0a44bb9
Reza <reza.jelodar@gmail.com>
2023-01-20 20:52:23 -0800

    Implement /repos API endpoint to use our RepoService

--- src/routes/repos.ts
+++ src/routes/repos.ts
@@ -1,12 +1,27 @@
 import { Router, Request, Response } from 'express';
+import RepoService from '../services/RepoService';
+import RemoteRepoFetcher from '../services/RemoteRepoFetcher';
+import RepoFilter from '../services/ForkRepoFilter';
+import RepoAggregator from '../services/RepoAggregator';
+import LocalRepoFetcher from '../services/LocalRepoFetcher';
 
-export const repos = Router();
+const repos = Router();
+const repoService = new RepoService(
+  new RepoAggregator([new RemoteRepoFetcher(), new LocalRepoFetcher()]),
+  new RepoFilter()
+);
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
-
-  res.status(200);
-
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  res.header('Content-Type', 'application/json');
+  try {
+    const data = await repoService.getData();
+    res.status(200).json(data);
+  } catch (err: any) {
+    res.status(500).json({
+      error: _.hostname === 'localhost' ? err.message : 'Data unavailable.',
+    });
+  }
 });
+
+export { repos };
